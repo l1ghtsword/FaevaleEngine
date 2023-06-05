42e90a5b9841ea13bf98a1c5ae965c41c3fb5ffa
L1ghtsword <l1ghtsword@hotmail.com>
2023-06-05 11:16:29 -0400

    Fixing Maven depends

--- src/main/java/ca/lightnet/mmoblockrespawn/MmoBlockRespawn.java
+++ src/main/java/ca/lightnet/mmoblockrespawn/MmoBlockRespawn.java
@@ -1,18 +1,20 @@
 package ca.lightnet.mmoblockrespawn;
 
+import ca.lightnet.mmoblockrespawn.listener.WildsBlockListener;
+import org.bukkit.Bukkit;
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/*
- * mmoblockrespawn java plugin
- */
-public class Plugin extends JavaPlugin
+public final class MmoBlockRespawn extends JavaPlugin
 {
   public static final Logger LOGGER=Logger.getLogger("mmoblockrespawn");
 
   public void onEnable()
   {
+    //Startup logic
     LOGGER.info("mmoblockrespawn enabled");
+    //Events
+    Bukkit.getServer().getPluginManager().registerEvents(new WildsBlockListener(), this);
   }
 
   public void onDisable()
--- src/main/java/ca/lightnet/mmoblockrespawn/listener/WildsBlockListener.java
+++ src/main/java/ca/lightnet/mmoblockrespawn/listener/WildsBlockListener.java
@@ -1,20 +1,14 @@
 package ca.lightnet.mmoblockrespawn.listener;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import com.palmergames.bukkit.towny.event.actions.TownyDestroyEvent;
-import ca.lightnet.mmoblockrespawn.Plugin;
+import ca.lightnet.mmoblockrespawn.MmoBlockRespawn;
 
 public class WildsBlockListener implements Listener {
-    public WildsBlockListener(Plugin Plugin) {
-        Bukkit.getPluginManager().registerEvents(this, Plugin);
-    }
-
-
     @EventHandler
     public void onWildsBlockBuild(TownyDestroyEvent event) {
-        Plugin.LOGGER.info(
+        MmoBlockRespawn.LOGGER.info(
                 "Event is cancelled? = " + event.isCancelled()+
                      " Wilgerness? = "+event.isInWilderness()+
                      " Player: "+event.getPlayer().getDisplayName()
